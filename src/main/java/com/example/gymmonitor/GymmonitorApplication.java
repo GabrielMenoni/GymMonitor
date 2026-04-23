b6e9cab1ab81f5ab01540bea8446614548a5b6ba
Gabriel Menoni <contato.gmenoni@gmail.com>
2026-04-23 14:59:42 -0300

    Initial release

---
+++ src/main/java/com/example/gymmonitor/GymmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gymmonitor/GymmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
